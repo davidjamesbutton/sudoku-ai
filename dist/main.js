a8e5fe81295b970d0844baf7abd415da4a34b6a7
davidjamesbutton <davidjamesbutton@users.noreply.github.com>
2024-10-05 11:27:30 +0000

    deploy: 255ddca6199a7eea36ed440cbb5c4f806ff7aa66

--- dist/main.js
+++
@@ -1,7 +0,0 @@
-import { SudokuGame } from './sudokuGame.js';
-// Expose SudokuGame to the global scope (window object)
-window.SudokuGame = SudokuGame;
-// Initialize the game when the window loads
-window.onload = () => {
-    new SudokuGame();
-};
